bdf30d985a886ee2cd2aee43e046bda4b37b84bb
jagerziel <fromnytosg@gmail.com>
2022-12-29 12:56:09 -0500

    LC_0242_Valid_Anagram_Solved-Slightly_More_Efficient

--- LC_0242_Valid_Anagram.js
+++ LC_0242_Valid_Anagram.js
@@ -29,31 +29,34 @@ Constraints:
  * @param {string} t
  * @return {boolean}
  */
+// //TEST 1
+// s = "anagram", t = "nagaram"
 
-s = "anagram", t = "nagaram"
+// //TEST2
+// s = "rat", t = "car"
 
-var isAnagram = function(s, t) {
-    //basic check
-    if (s.length !== t.length) {
-        return false
-    }
-    //convert t into array
-    t = t.split("")
-    //loop through each letter of s
-    for (let i = 0; i < s.length; i++) {
-        //check if letter in s is present in t
-        if (s[i] = t[t.indexOf(s[i])]) {
-            //remove letter from t if present
-            t.splice(t.indexOf(s[i]),1)
-        } else {
-            //return false if letter not present
-            return false
-        }
-    }
-    return true
-};
+// var isAnagram = function(s, t) {
+//     //basic check
+//     if (s.length !== t.length) {
+//         return false
+//     }
+//     //convert t into array
+//     t = t.split("")
+//     //loop through each letter of s
+//     for (let i = 0; i < s.length; i++) {
+//         //check if letter in s is present in t
+//         if (s[i] = t[t.indexOf(s[i])]) {
+//             //remove letter from t if present
+//             t.splice(t.indexOf(s[i]),1)
+//         } else {
+//             //return false if letter not present
+//             return false
+//         }
+//     }
+//     return true
+// };
 
-console.log(isAnagram(s, t))
+// console.log(isAnagram(s, t))
 
 /*
 Runtime4041 ms
@@ -65,6 +68,41 @@ Beats
 */
 
 /*
-WORKING ON MORE EFFICIENT SOLUTION
+SECOND SOLUTION - SLIGHTLY MORE EFFICIENT
 */
 
+var isAnagram = function(s, t) {
+    //basic check
+    if (s.length !== t.length) {
+        return false
+    }
+    //convert t into array
+    t = t.split("")
+    //Loop through s
+    for (let i = 0; i < s.length; i++) {
+        //Loop through t to find match to s
+        for (let j = 0; j < t.length; j++) {
+            //If match found, remove item from j and break loop
+            if (s[i] === t[j]) {
+                t.splice(j, 1)
+                break
+            //If no matches found by end of loop, return false
+            } else if (j === t.length - 1) {
+                return false
+            }
+        }
+    }
+    //If all loops complete successfully, return result
+    return true
+}
+
+console.log(isAnagram(s, t))
+
+/*
+Runtime2573 ms
+Beats
+5.1%
+Memory46.5 MB
+Beats
+35.86%
+*/
