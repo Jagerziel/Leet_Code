1cfc321e1c6ccf4630a10f22f5a04937cdece606
jagerziel <fromnytosg@gmail.com>
2022-12-29 13:45:30 -0500

    LC_0242_Valid_Anagram_Solved-Third_Solution_Slightly__More_Efficient_Than_Two

--- LC_0242_Valid_Anagram.js
+++ LC_0242_Valid_Anagram.js
@@ -59,50 +59,89 @@ Constraints:
 // console.log(isAnagram(s, t))
 
 /*
-Runtime4041 ms
-Beats
-5.1%
-Memory51.2 MB
-Beats
-5.4%
+Runtime 4041 ms
+Beats 5.1%
+Memory 51.2 MB
+Beats 5.4%
 */
 
 /*
 SECOND SOLUTION - SLIGHTLY MORE EFFICIENT
 */
 
+// var isAnagram = function(s, t) {
+//     //basic check
+//     if (s.length !== t.length) {
+//         return false
+//     }
+//     //convert t into array
+//     t = t.split("")
+//     //Loop through s
+//     for (let i = 0; i < s.length; i++) {
+//         //Loop through t to find match to s
+//         for (let j = 0; j < t.length; j++) {
+//             //If match found, remove item from j and break loop
+//             if (s[i] === t[j]) {
+//                 t.splice(j, 1)
+//                 break
+//             //If no matches found by end of loop, return false
+//             } else if (j === t.length - 1) {
+//                 return false
+//             }
+//         }
+//     }
+//     //If all loops complete successfully, return result
+//     return true
+// }
+
+// console.log(isAnagram(s, t))
+
+/*
+Runtime 2573 ms
+Beats 5.1%
+Memory 46.5 MB
+Beats 35.86%
+*/
+
+/*
+THIRD SOLUTION - MUCH FASTER (but still not the best - may come back to this)
+*/
+
+// s = s.split("")
+// console.log(s.filter((char) => char !== 'a'))
+
+// //TEST 1
+s = "anagram", t = "nagaram"
+
+// //TEST2
+// s = "rat", t = "car"
+
 var isAnagram = function(s, t) {
     //basic check
     if (s.length !== t.length) {
         return false
     }
-    //convert t into array
+    //split strings into arrays
+    s = s.split("")
     t = t.split("")
-    //Loop through s
-    for (let i = 0; i < s.length; i++) {
-        //Loop through t to find match to s
-        for (let j = 0; j < t.length; j++) {
-            //If match found, remove item from j and break loop
-            if (s[i] === t[j]) {
-                t.splice(j, 1)
-                break
-            //If no matches found by end of loop, return false
-            } else if (j === t.length - 1) {
-                return false
-            }
+    //conduct loop to filter out each letter
+    while (s.length > 0) {
+        //filter out current letter in s string
+        t = t.filter((char) => char !== s[0])
+        s = s.filter((char) => char !== s[0])
+        //if the lengths of each string differ, return false
+        if (s.length !== t.length) {
+            return false
         }
     }
-    //If all loops complete successfully, return result
+    //if both strings filter to 0, return true
     return true
-}
-
+};
 console.log(isAnagram(s, t))
 
 /*
-Runtime2573 ms
-Beats
-5.1%
-Memory46.5 MB
-Beats
-35.86%
+Runtime 115 ms
+Beats 62.59%
+Memory 57.5 MB
+Beats 5.4%
 */
