3f3295a524e20ba306a6ca7affcc7e73dbc9a3fc
jagerziel <fromnytosg@gmail.com>
2022-12-31 03:17:03 -0500

    LC_0482_License_Key_Formatting_Solved-Slightly_Simplier_Code

--- LC_0482_License_Key_Formatting.js
+++ LC_0482_License_Key_Formatting.js
@@ -47,43 +47,64 @@ s = "5F3Z-2e-9-w", k = 4
 // TEST4
 // s = "3-J", k = 4
 
-test2 = "99q"
-console.log(test2.toUpperCase())
+// var licenseKeyFormatting = function(s, k) {
+//     //Split String to array
+//     s = s.split('')
+//     //Count indexes until k
+//     let counter = 0
+//     //Loop through string
+//     for (let i = s.length; i >= 0; i--) {
+//         //Remove any current instances of '-'
+//         if (s[i] === '-') {
+//             s.splice(i, 1)
+//         } else {
+//             //Insert '-' when counter equals k, reset counter
+//             if (counter === k) {
+//                 s.splice(i,0,'-')
+//                 counter = 0
+//             }
+//             counter++
+//         }
+//     }
+//     //Remove any leading '-' (occurs when 'k' evenly divides into the cleansed string)
+//     if (s[0] === '-') {
+//         s.splice(0, 1)
+//     }
+//     //Convert back to string with upper-case letters
+//     s = s.join('').toUpperCase()
+//     //Return result
+//     return s
+// };
 
-var licenseKeyFormatting = function(s, k) {
-    //Split String to array
-    s = s.split('')
-    //Count indexes until k
-    let counter = 0
-    //Loop through string
-    for (let i = s.length; i >= 0; i--) {
-        //Remove any current instances of '-'
-        if (s[i] === '-') {
-            s.splice(i, 1)
-        } else {
-            //Insert '-' when counter equals k, reset counter
-            if (counter === k) {
-                s.splice(i,0,'-')
-                counter = 0
-            }
-            counter++
-        }
-    }
-    //Remove any leading '-' (occurs when 'k' evenly divides into the cleansed string)
-    if (s[0] === '-') {
-        s.splice(0, 1)
-    }
-    //Convert back to string with upper-case letters
-    s = s.join('').toUpperCase()
-    //Return result
-    return s
-};
-
-console.log(licenseKeyFormatting(s, k))
+// console.log(licenseKeyFormatting(s, k))
 
 /*
 Runtime 303 ms
 Beats 13.74%
 Memory 49.3 MB
 Beats 31.4%
+*/
+
+/*
+ATTEMPT 2: SLIGHTLY SIMPLIER CODE BUT SAME RUNTIME
+*/
+
+var licenseKeyFormatting = function(s, k) {
+    //Clean string and convert to uppercase
+    s = s.replaceAll('-','').toUpperCase()
+    //Iterate through string in reverse inserting '-' at position k
+    for (let i = s.length; i > 0; i -= k) {
+        s = s.slice(0, i) + '-' + s.slice(i)
+    }
+    //Return result
+    return s.slice(0, s.length - 1)
+};
+
+console.log(licenseKeyFormatting(s, k))
+
+/*
+Runtime 302 ms
+Beats 13.74%
+Memory 47.8 MB
+Beats 54.96%
 */
