95230ba5c296ca3c807ee3ab6e1ab95f3be0a403
Jagerziel <fromnytosg@gmail.com>
2023-08-13 12:38:24 -0400

    LC_1071_Greatest_Common_Divisor_of_Strings_Solved_OPTIMAL

--- LC_1071_Greatest_Common_Divisor_of_Strings.js
+++ LC_1071_Greatest_Common_Divisor_of_Strings.js
@@ -39,6 +39,49 @@ let test7 = "ABABABAB", test8 = "ABAB"
 
 // ATTEMPT 1: FIND ARRAY OF LONGEST MATCHING STRINGS FROM SHORTEST STRING AND COMPARE TO LONGEST
 
+// var gcdOfStrings = function(str1, str2) {
+//     // Track short and long string as well as their relative lengths
+//     let shortString = ""
+//     let longString = ""
+
+//     if (str1.length >= str2.length) {
+//         shortString = str2
+//         longString = str1
+//     } else {
+//         shortString = str1
+//         longString = str2
+//     }
+//     let shortLen = shortString.length
+//     let longLen = longString.length
+//     // Track results of pattern from short string from longest match to shortest
+//     shortStringPossibilities = []
+//     // Add patters to shortString possibilitites
+//     for (let i = 0; i < shortLen; i++) {
+//         if (shortLen % (i + 1) === 0) {
+//             if (shortString.slice(0, i + 1).repeat(shortLen / (i + 1)) === shortString) {
+//                 shortStringPossibilities.unshift(shortString.slice(0, i + 1))
+//             }
+//         }
+//     }
+//     // Check shorStringPossibilities to see if they match within the longer string.  If so, return first instance.
+//     for (let i = 0; i < shortStringPossibilities.length; i++) {
+//         if ( longLen % shortStringPossibilities[i].length === 0) {
+//             if (longString === shortStringPossibilities[i].repeat(longLen / shortStringPossibilities[i].length)) return shortStringPossibilities[i]
+//         }
+//     }
+//     // If no match, return ""
+//     return ""
+// };
+
+/*
+Runtime 57 ms
+Beats 69.13%
+Memory 44.4 MB
+Beats 27.16%
+*/
+
+// ATTEMPT 2: REMOVE NEED FOR SHORT STRING TO GO THROUGH FULL LOOP
+
 var gcdOfStrings = function(str1, str2) {
     // Track short and long string as well as their relative lengths
     let shortString = ""
@@ -56,13 +99,14 @@ var gcdOfStrings = function(str1, str2) {
     // Track results of pattern from short string from longest match to shortest
     shortStringPossibilities = []
     // Add patters to shortString possibilitites
-    for (let i = 0; i < shortLen; i++) {
+    for (let i = 0; i < shortLen / 2; i++) {
         if (shortLen % (i + 1) === 0) {
             if (shortString.slice(0, i + 1).repeat(shortLen / (i + 1)) === shortString) {
                 shortStringPossibilities.unshift(shortString.slice(0, i + 1))
             }
         }
     }
+    shortStringPossibilities.unshift(shortString)
     // Check shorStringPossibilities to see if they match within the longer string.  If so, return first instance.
     for (let i = 0; i < shortStringPossibilities.length; i++) {
         if ( longLen % shortStringPossibilities[i].length === 0) {
@@ -74,10 +118,55 @@ var gcdOfStrings = function(str1, str2) {
 };
 
 /*
-Runtime 57 ms
-Beats 69.13%
-Memory 44.4 MB
+Runtime 56 ms
+Beats 72.94%
+Memory 44.5 MB
 Beats 27.16%
+*/
+
+// ATTEMPT 3: PUT shortStringPossibilities[i] into a variable
+
+var gcdOfStrings = function(str1, str2) {
+    // Track short and long string as well as their relative lengths
+    let shortString = ""
+    let longString = ""
+
+    if (str1.length >= str2.length) {
+        shortString = str2
+        longString = str1
+    } else {
+        shortString = str1
+        longString = str2
+    }
+    let shortLen = shortString.length
+    let longLen = longString.length
+    // Track results of pattern from short string from longest match to shortest
+    shortStringPossibilities = []
+    // Add patters to shortString possibilitites
+    for (let i = 0; i < shortLen / 2; i++) {
+        if (shortLen % (i + 1) === 0) {
+            if (shortString.slice(0, i + 1).repeat(shortLen / (i + 1)) === shortString) {
+                shortStringPossibilities.unshift(shortString.slice(0, i + 1))
+            }
+        }
+    }
+    shortStringPossibilities.unshift(shortString)
+    // Check shorStringPossibilities to see if they match within the longer string.  If so, return first instance.
+    for (let i = 0; i < shortStringPossibilities.length; i++) {
+        if ( longLen % shortStringPossibilities[i].length === 0) {
+            let currStr = shortStringPossibilities[i]
+            if (longString === currStr.repeat(longLen / currStr.length)) return currStr
+        }
+    }
+    // If no match, return ""
+    return ""
+};
+
+/*
+Runtime 49 ms
+Beats 91.70%
+Memory 44.1 MB
+Beats 32.85%
 */
 
 
