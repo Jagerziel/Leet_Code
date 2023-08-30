5d249a3d4564109dfd6e8631c51cc1b77198065f
Jagerziel <fromnytosg@gmail.com>
2023-08-30 12:58:26 -0400

    LC_0227_Basic_Calculator_II_Solved_NOT_OPTIMAL_BUT_CLOSER

--- LC_0227_Basic_Calculator_II.js
+++ LC_0227_Basic_Calculator_II.js
@@ -48,22 +48,88 @@ let test1 = "3+2*2", // 7
     test6 = "433     ", // 433
     test7 = " 25 * 31   " // 775
 
+// ATTEMPT 1: CLEAN DATA, HANDLE MULT/DIV FIRST, HANDLE ADD/SUBT
+// var calculate = function(s) {
+//     function operationMultDiv(num1, num2, symbol) {
+//         if (symbol === "*") return num1 * num2
+//         if (symbol === "/") return Math.trunc(num1 / num2)
+//     }
+//     function operationAddSubt(num1, num2, symbol) {
+//         if (symbol === "+") return num1 + num2
+//         if (symbol === "-") return num1 - num2
+//     }
+//     let number = ""
+//     let calcArr = []
+//     for (let i = 0; i < s.length; i++) {
+//         if (s[i] === " ") continue
+//         if (isNaN(parseInt(s[i]))) {
+//             calcArr.push(parseInt(number))
+//             calcArr.push(s[i])
+//             number = ""
+//         } else {
+//             number += s[i]
+//         }
+//     }
+//     calcArr.push(parseInt(number))
+
+//     let calcArrLen = calcArr.length
+//     let calcArr2 = []
+//     let contEval = false
+//     let contEvalVal = 0
+//     for (let i = 1; i < calcArrLen; i += 2) {
+//         if (calcArr[i] === "*" || calcArr[i] === "/") {
+//             if (contEval === false) {
+//                 contEvalVal = operationMultDiv(calcArr[i - 1], calcArr[i + 1], calcArr[i])
+//                 contEval = true
+//             } else {
+//                 contEvalVal = operationMultDiv(contEvalVal, calcArr[i + 1], calcArr[i])
+//             }
+//         } else {
+//             if (contEval === false) {
+//                 calcArr2.push(calcArr[i - 1])
+//                 calcArr2.push(calcArr[i])
+//             } else {
+//                 calcArr2.push(contEvalVal)
+//                 calcArr2.push(calcArr[i])
+//                 contEval = false
+//             }
+//         }
+//     }
+//     if (contEval) calcArr2.push(contEvalVal)
+//     if (!contEval) calcArr2.push(calcArr[calcArrLen - 1])
+
+//     contEvalVal = calcArr2[0]
+
+//     for (let i = 1; i < calcArr2.length; i += 2) {
+//         contEvalVal = operationAddSubt(contEvalVal , calcArr2[i + 1] , calcArr2[i]) 
+//     }
+
+//     return contEvalVal
+// };
+
+/*
+Runtime 90 ms
+Beats 71.39%
+Memory 67.1 MB
+Beats 11.49%
+*/
+
+
+// ATTEMPT 2: CLEAN FUNCTIONS AND OPTIMIZE
+
 var calculate = function(s) {
     function operationMultDiv(num1, num2, symbol) {
-        if (symbol === "*") return num1 * num2
-        if (symbol === "/") return Math.trunc(num1 / num2)
+        return symbol === "*" ? num1 * num2 : Math.trunc(num1 / num2)
     }
     function operationAddSubt(num1, num2, symbol) {
-        if (symbol === "+") return num1 + num2
-        if (symbol === "-") return num1 - num2
+        return symbol === "+" ? num1 + num2 : num1 - num2
     }
-    let number = ""
-    let calcArr = []
+    let number = "",
+        calcArr = []
     for (let i = 0; i < s.length; i++) {
         if (s[i] === " ") continue
         if (isNaN(parseInt(s[i]))) {
-            calcArr.push(parseInt(number))
-            calcArr.push(s[i])
+            calcArr.push(parseInt(number), s[i])
             number = ""
         } else {
             number += s[i]
@@ -71,10 +137,12 @@ var calculate = function(s) {
     }
     calcArr.push(parseInt(number))
 
-    let calcArrLen = calcArr.length
-    let calcArr2 = []
-    let contEval = false
-    let contEvalVal = 0
+    if (calcArr.length === 1) return calcArr[0]
+
+    let calcArrLen = calcArr.length,
+        calcArr2 = [],
+        contEval = false,
+        contEvalVal = 0
     for (let i = 1; i < calcArrLen; i += 2) {
         if (calcArr[i] === "*" || calcArr[i] === "/") {
             if (contEval === false) {
@@ -85,19 +153,15 @@ var calculate = function(s) {
             }
         } else {
             if (contEval === false) {
-                calcArr2.push(calcArr[i - 1])
-                calcArr2.push(calcArr[i])
+                calcArr2.push(calcArr[i - 1], calcArr[i])
             } else {
-                calcArr2.push(contEvalVal)
-                calcArr2.push(calcArr[i])
+                calcArr2.push(contEvalVal, calcArr[i])
                 contEval = false
             }
         }
     }
     if (contEval) calcArr2.push(contEvalVal)
-    if (!contEval) calcArr2.push(calcArr[calcArrLen - 1])
-
-    // console.log(calcArr2)
+    else calcArr2.push(calcArr[calcArrLen - 1])
 
     contEvalVal = calcArr2[0]
 
@@ -109,13 +173,11 @@ var calculate = function(s) {
 };
 
 /*
-Runtime 90 ms
-Beats 71.39%
-Memory 67.1 MB
-Beats 11.49%
+Runtime 87 ms
+Beats 76.53%
+Memory 68.2 MB
+Beats 10.2%
 */
-
-
 
 console.log(calculate(test1))
 console.log(calculate(test2))
