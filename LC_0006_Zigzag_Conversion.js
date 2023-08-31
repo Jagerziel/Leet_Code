896e0789651811bf7e1912023fd83c2ed263c678
Jagerziel <fromnytosg@gmail.com>
2023-08-31 12:07:05 -0400

    LC_0006_Zigzag_Conversion_Solved_OPTIMAL

--- LC_0006_Zigzag_Conversion.js
+++ LC_0006_Zigzag_Conversion.js
@@ -62,7 +62,7 @@ let test05s = "AB",
 let test06s = "ABCDEF",
     test06r = 4
 
-
+// ATTEMPT 1: ZIG ZAG THROUGH ARRAY MOVING FROM TOP INDEX TO NUMROWS
 
 var convert = function(s, numRows) {
     let resultArr = []
@@ -119,6 +119,69 @@ Runtime 73 ms
 Beats 70.82%
 Memory 47.9 MB
 Beats 48.69%
+*/
+
+// ATTEMPT 2: SHORTCUT WHERE LIMIT === 0
+
+var convert = function(s, numRows) {
+    // Set variables
+    let resultArr = []
+    let sLen = s.length
+    let limit = (s.length - 1) % (numRows - 1)
+    let whileCt = numRows
+    let direction = true 
+    // Set early resolve conditions
+    if (numRows === 1) return s
+    if (sLen <= numRows) return s
+    // Complete first row down and set initial values of resultArr
+    for (let i = 0; i < numRows; i++) {
+        resultArr.push(s[i])
+    }
+    // Depending on direction, add letter to each index of array via zig-zag
+    while (whileCt < sLen - limit) {
+        if (direction) {
+            for (let i = numRows - 2; i >= 0; i--) {
+                resultArr[i] += s[whileCt]
+                whileCt++
+            }
+            direction = false
+        } else {
+            for (let i = 1; i < numRows; i++) {
+                resultArr[i] += s[whileCt]
+                whileCt++
+            }
+            direction = true
+        }
+    }
+    // If there are any remaining letters that did not concisely fit within a full zig (or zag), add to the array results
+    if (limit !== 0) {
+        if (direction) {
+            for (let i = 1; i <= limit; i++) {
+                resultArr[numRows - i - 1] += s[whileCt]
+                whileCt++
+                
+            }
+        } else {
+            for (let i = 1; i <= limit; i++) {
+                resultArr[i] += s[whileCt]
+                whileCt++
+            }
+        }
+    }
+    // Concatenate array indexes into a string
+    let result = ""
+    for (let i of resultArr) {
+        result += i
+    }
+    // Return result
+    return result
+};
+
+/*
+Runtime 62 ms
+Beats 93.70%
+Memory 47.5 MB
+Beats 50.27%
 */
 
 // console.log(convert(test01s, test01r))
