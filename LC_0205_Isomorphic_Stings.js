10e45981eff504cf859bf5fa8fa985d34f48447a
Jagerziel <fromnytosg@gmail.com>
2023-08-31 13:35:10 -0400

    LC_0205_Isomorphic_Stings_Solved_OPTIMAL

--- LC_0205_Isomorphic_Stings.js
+++ LC_0205_Isomorphic_Stings.js
@@ -38,26 +38,29 @@ let s = "egg", t = "add"
  * @param {string} t
  * @return {boolean}
  */
-var isIsomorphic = function(s, t) {
-    let test_s = {}
-    let test_t = {}
 
-    for (let i = 0; i < s.length; i++) {
-        if (test_s[s[i]] === undefined) {
-            test_s[s[i]] = t[i]
-            if (test_t[t[i]] === undefined) {
-                test_t[t[i]] = s[i]
-            } else {
-                return false
-            }
-        } else {
-            if (test_s[s[i]] !== t[i]) {
-                return false
-            }
-        }
-    }
-    return true
-};
+// ATTEMPT 1: MATCHING KEYS AND OBJECT VALUES
+
+// var isIsomorphic = function(s, t) {
+//     let test_s = {}
+//     let test_t = {}
+
+//     for (let i = 0; i < s.length; i++) {
+//         if (test_s[s[i]] === undefined) {
+//             test_s[s[i]] = t[i]
+//             if (test_t[t[i]] === undefined) {
+//                 test_t[t[i]] = s[i]
+//             } else {
+//                 return false
+//             }
+//         } else {
+//             if (test_s[s[i]] !== t[i]) {
+//                 return false
+//             }
+//         }
+//     }
+//     return true
+// };
 
 /*
 Runtime 68 ms
@@ -66,5 +69,38 @@ Memory 42.1 MB
 Beats 84.30%
 */
 
+// ATTEMPT 2: REMOVE NEEDLESS PULL OF s[i] to insert into definitions of t[t[i]]
+
+var isIsomorphic = function(s, t) {
+    // Track Objects for s and t 
+    let test_s = {}
+    let test_t = {}
+    // Loop through the index of both s & t simultaneously 
+    for (let i = 0; i < s.length; i++) {
+        if (test_s[s[i]] === undefined) {
+            if (test_t[t[i]] !== undefined) return false // Exit if 't' already has a definition as it is stored duplicatively under s
+            test_s[s[i]] = t[i]
+            test_t[t[i]] = "1"
+        } else if (test_s[s[i]] !== t[i]) { // Exit if the duplicate of s does not match current pos of t
+            return false
+        }
+    }
+    // Return result
+    return true
+};
+
+/*
+Runtime 58 ms
+Beats 86.43%
+Memory 42.3 MB
+Beats 80.38%
+*/
 
 console.log(isIsomorphic(s, t))
+
+let a = 2
+let b = 3
+
+if (a === 2 || b === 3) {
+    console.log(true)
+}
