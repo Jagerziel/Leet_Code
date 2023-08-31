abdf35b1896030dd76ec4d9d33dc92d06fbb64eb
Jagerziel <fromnytosg@gmail.com>
2023-08-31 11:57:55 -0400

    LC_0006_Zigzag_Conversion_Solved_NOT_OPTIMAL

---
+++ LC_0006_Zigzag_Conversion.js
@@ -0,0 +1,129 @@
+/*
+6. Zigzag Conversion
+Medium
+6.6K
+13.1K
+Companies
+
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+
+And then read line by line: "PAHNAPLSIIGYIR"
+Write the code that will take a string and make this conversion given a number of rows:
+string convert(string s, int numRows);
+
+Example 1:
+Input: s = "PAYPALISHIRING", numRows = 3
+Output: "PAHNAPLSIIGYIR"
+
+Example 2:
+Input: s = "PAYPALISHIRING", numRows = 4
+Output: "PINALSIGYAHRPI"
+Explanation:
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+
+Example 3:
+Input: s = "A", numRows = 1
+Output: "A"
+
+Constraints:
+    1 <= s.length <= 1000
+    s consists of English letters (lower-case and upper-case), ',' and '.'.
+    1 <= numRows <= 1000
+*/
+
+/*
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+
+let test01s = "PAYPALISHIRING",
+    test01r = 3
+
+let test02s = "PAYPALISHIRING",
+    test02r = 4
+
+let test03s = "A",
+    test03r = 3
+
+let test04s = "APPLETHINGSY",
+    test04r = 3
+
+let test05s = "AB",
+    test05r = 1
+
+let test06s = "ABCDEF",
+    test06r = 4
+
+
+
+var convert = function(s, numRows) {
+    let resultArr = []
+    let sLen = s.length
+    let limit = (s.length - 1) % (numRows - 1)
+    let whileCt = numRows
+    let direction = true // true=diagonal-up; false=down
+
+    if (numRows === 1) return s
+    if (sLen <= numRows) return s
+
+    for (let i = 0; i < numRows; i++) {
+        resultArr.push(s[i])
+    }
+
+    while (whileCt < sLen - limit) {
+        if (direction) {
+            for (let i = numRows - 2; i >= 0; i--) {
+                resultArr[i] += s[whileCt]
+                whileCt++
+            }
+            direction = false
+        } else {
+            for (let i = 1; i < numRows; i++) {
+                resultArr[i] += s[whileCt]
+                whileCt++
+            }
+            direction = true
+        }
+    }
+
+    if (direction) {
+        for (let i = 1; i <= limit; i++) {
+            resultArr[numRows - i - 1] += s[whileCt]
+            whileCt++
+            
+        }
+    } else {
+        for (let i = 1; i <= limit; i++) {
+            resultArr[i] += s[whileCt]
+            whileCt++
+        }
+    }
+
+    let result = ""
+    for (let i of resultArr) {
+        result += i
+    }
+    return result
+};
+
+/*
+Runtime 73 ms
+Beats 70.82%
+Memory 47.9 MB
+Beats 48.69%
+*/
+
+// console.log(convert(test01s, test01r))
+// console.log(convert(test02s, test02r))
+// console.log(convert(test03s, test03r))
+// console.log(convert(test04s, test04r))
+// console.log(convert(test05s, test05r))
+console.log(convert(test06s, test06r))
